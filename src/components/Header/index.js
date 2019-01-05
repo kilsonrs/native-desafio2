28533e3b9c4ab83e4297d8e390b3aeb05eb8dbe4
Kilson <kilsonrs@gmail.com>
2019-01-05 12:02:26 -0200

    Navigation

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+const Header = () => <View />;
+
+export default Header;
--- src/pages/Repositorios/index.js
+++ src/pages/Repositorios/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+// import api from '~/services/api';
 import { View, Text } from 'react-native';
 
 // import styles from './styles';
 
-const Repositorios = () => (
-  <View>
-    <Text>repositorios</Text>
-  </View>
-);
+class Repositorios extends Component {
+  state = {
+    repositoryItem: [],
+  };
+
+  loadRepositorios = () => {};
+
+  render() {
+    return (
+      <View>
+        <View style={styles.repoItem}>
+          <Text style={styles.title}>rocketnative</Text>
+          <Text style={styles.organization}>RocketSeat</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default Repositorios;
